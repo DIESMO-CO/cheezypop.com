22aa98ddf51c56b90eb052e23e4deb2401d7e77a
rileydturner <rileydturner41@gmail.com>
2022-03-07 18:56:25 -0800

    Basic Sign up page

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ app = Flask(__name__);
 def home():
     return render_template("index.html");
 
-@app.route("/submission")
+@app.route("/signup")
 def submission():
-    return render_template("submission.html");
+    return render_template("signup.html");
 
 if __name__ == "__main__":
     app.run();
