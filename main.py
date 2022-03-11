9cd182d717872632508fdc48fe09b8b488db6d7f
rileydturner <rileydturner41@gmail.com>
2022-03-10 18:22:09 -0800

    Added login into main.py

--- main.py
+++ main.py
@@ -6,9 +6,26 @@ app = Flask(__name__);
 def home():
     return render_template("index.html");
 
-@app.route("/login")
+@app.route("/submission")
 def submission():
+    return render_template("submission.html");
+
+@app.route("/signup")
+def signup():
+    return render_template("signup.html");
+
+@app.route("/login")
+def moviePage():
     return render_template("login.html");
+
+@app.route("/movies")
+def movies():
+    return render_template("movies.html");
+
+@app.route("/movie-page")
+def moviePage():
+    return render_template("movie-page.html");
+
 
 if __name__ == "__main__":
     app.run();
